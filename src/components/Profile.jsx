30560491260e0c8b1c7134014be220e6f018ea75
Eduardo Godoy <edo.godoy88@gmail.com>
2025-03-06 23:00:02 -0300

    se modifica Profile.jsx para la autenticacion

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Profile = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem('token')
+
+      if (token) {
+        try {
+          const response = await axios.get("http://localhost:5000/api/auth/me", {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          })
+          console.log('Api Response: ', response.data)
+          setUser(response.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    fetchUser()
+  }, [])
+
   return (
     <div>
-      <h2>Perfil de Usuario</h2>
-      <p>Email: email@usuario.com</p>
-      <button>Cerrar Sesión</button>
+      {user
+        ? (
+          <div>
+            <div>
+              <h3>Perfil de Usuario</h3>
+            </div>
+            <div>
+              <div>
+                <img
+                  src='https://www.shutterstock.com/image-illustration/david-street-style-graphic-designtextile-600nw-2265632523.jpg'
+                  alt='User Avatar'
+                />
+                <h5><strong>Email:</strong></h5>
+                <p>{user.email}</p>
+              </div>
+              <hr />
+            </div>
+          </div>
+          )
+        : (
+          <div>
+            <p><strong>Por favor, inicia sesión para ver tu perfil.</strong></p>
+          </div>
+          )}
     </div>
   )
 }
